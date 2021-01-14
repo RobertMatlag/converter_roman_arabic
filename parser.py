dd10ef52b98884b9c05fbe0bd8e729379707cef8
RobertMatlag <robert.matlag@gmail.com>
2021-01-14 14:46:11 +0100

    Recognize middle roman numbers

--- parser.py
+++ parser.py
@@ -1,12 +1,32 @@
+import re
+
+
+# pattern = re.compile(r'^[I]{1,3}$')
+# matches = pattern.finditer(s)
+# match = next(matches)
+# print(match)
+
 def parse_roman_numeral(s):
-    roman_arabic_dict = {'I': 1}
+    roman_arabic_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50}
 
     index = 0
     value = 0
     while index < len(s):
         sign = s[index]
-        value += roman_arabic_dict[sign]
-        index += 1
+        if index == len(s) - 1:
+            value += roman_arabic_dict[sign]
+            index += 1
+        else:
+            next_sign = s[index + 1]
+            first_value = roman_arabic_dict[sign]
+            second_value = roman_arabic_dict[next_sign]
+
+            if second_value > first_value:
+                value += (second_value - first_value)
+                index += 2
+            else:
+                value += roman_arabic_dict[sign]
+                index += 1
 
     return value
 
--- parser_test.py
+++ parser_test.py
@@ -8,6 +8,16 @@ class RomanNumeralsTest(unittest.TestCase):
         self.assertEqual(parse_roman_numeral("II"), 2)
         self.assertEqual(parse_roman_numeral("III"), 3)
 
+    def test_middle_numbers(self):
+        self.assertEqual(parse_roman_numeral("IV"), 4)
+        self.assertEqual(parse_roman_numeral("V"), 5)
+        self.assertEqual(parse_roman_numeral("VI"), 6)
+        self.assertEqual(parse_roman_numeral("VIII"), 8)
+        self.assertEqual(parse_roman_numeral("IX"), 9)
+        self.assertEqual(parse_roman_numeral("X"), 10)
+        self.assertEqual(parse_roman_numeral("XLVII"), 47)
+        self.assertEqual(parse_roman_numeral("LXXXIX"), 89)
+
 
 if __name__ == '__main__':
     unittest.main()
